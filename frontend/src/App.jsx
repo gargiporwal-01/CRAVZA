1b19f60d724149d1741a2fc86dec740c311d82b1
Gargi Porwal <gargi.porwal.2828@gmail.com>
2025-09-14 15:02:54 +0530

    Explore Menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Home from './pages/Home/Home'   
+import Cart from './pages/Cart/Cart' 
 
 const App = () => {
   return (
